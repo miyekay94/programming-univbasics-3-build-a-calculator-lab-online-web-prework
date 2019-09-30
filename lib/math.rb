0eec56c5a97df63c87d528546c09a655abb07c66
Mary Kanu <maryikanu@gmail.com>
2019-09-30 04:05:05 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,47 @@
-def addition(num1, num2)
+val1 = gets.to_i
+sym = gets.chomp
+val2 = gets.to_i
 
+def addition(val1, val2)
+    return val1+val2
+end
+
+add = addition(val1, val2)
+
+def subtraction(val1, val2)
+    return val1-val2
+end 
+
+sub = subtraction(val1, val2)
+
+def division(val1, val2)
+    return val1/val2
+end 
+
+div = division(val1, val2)
+
+def multiplication(val1, val2)
+    return val1*val2
+end
+
+def addition(num1, num2)
+return num1+num2
 end
 
 def subtraction(num1, num2)
-
+return num1-num2
 end
 
 def division(num1, num2)
-
+return num1/num2
 end
 
 def multiplication(num1, num2)
-
+return math.sqrt num1*num2
 end
 
 def modulo(num1, num2)
-
+return num1%num2
 end
 
 def square_root(num)

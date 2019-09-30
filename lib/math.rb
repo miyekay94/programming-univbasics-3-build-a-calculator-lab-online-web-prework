b45481ba36fe1451b024c3082dc3fc39ef9a8099
Mary Kanu <maryikanu@gmail.com>
2019-09-30 04:03:56 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,3 +1,29 @@
+val1 = gets.to_i
+sym = gets.chomp
+val2 = gets.to_i
+
+def addition(val1, val2)
+    return val1+val2
+end
+
+add = addition(val1, val2)
+
+def subtraction(val1, val2)
+    return val1-val2
+end 
+
+sub = subtraction(val1, val2)
+
+def division(val1, val2)
+    return val1/val2
+end 
+
+div = division(val1, val2)
+
+def multiplication(val1, val2)
+    return val1*val2
+end
+
 def addition(num1, num2)
 return num1+num2
 end
